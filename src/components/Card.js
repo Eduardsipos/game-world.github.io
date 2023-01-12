6f9636af288185370981e2e273234822940eeaca
Eduardsipos <104792804+Eduardsipos@users.noreply.github.com>
2023-01-12 09:58:41 +0200

    Changing style

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({name, description, id, url}) =>{
     
     return(
-        <div className="tc bg-light-green dib br3 ma2 grow bw2 shadow-10">
+        <div className="tc bg-dark-green dib br3 ma2 grow bw2 shadow-10">
             <a href={url}  > 
             <img alt="Games" src={`https://robohash.org/set=set3${id}?100x100`} />
             </a>
